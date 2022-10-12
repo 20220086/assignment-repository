da01a343e3c6ddf026f125ab83f56080c0525ca7
20220086 <kilbddg@icloud.com>
2022-10-12 13:01:15 +0900

    1012 update2

--- multiplication.py
+++ multiplication.py
@@ -1,5 +1,7 @@
-
-for i in range(1,10):
-    print(i+1)
-    for j in range(1,10):
-        print(i, " * ", j, " = ", i*j)
+# 구구단
+def print9x9():
+    for i in range(1,10):
+        print(i)
+        for j in range(1,10):
+            print(i, " * ", j, " = ", i*j)
+print9x9()
